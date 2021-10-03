811848a79a4ac5f722f76f6987c62c2ad48141e9
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-10-04 00:27:37 +0900

    Create WordRelay.jsx

---
+++ lecture/WordRelay.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const { Module } = require('webpack');
+const {comonent} = React;
+
+class WordRelay extends React.Component {
+    state = {
+
+    };
+
+    render() {
+
+    }
+}
+
+module.exports = WordRelay;
