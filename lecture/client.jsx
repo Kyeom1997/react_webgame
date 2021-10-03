bca073a9c3954f618aa11e103de1e48ac720b877
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-10-04 00:27:33 +0900

    webpack

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,2 +1,5 @@
 const React = require('react');
 const ReactDom = require('react-dom');
+const WordRelay = require('./wordRelay');
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  name: "wordrelay-setting",
+  mode: "development", //실서비스 : production
+  devtool: "eval", //빠르게
+
+  entry: {
+    app: ["./client.jsx", "WordRelay.jsx"],
+  }, //입력
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, //출력
+};
