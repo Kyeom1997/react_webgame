1139a9d11a934796cddfd8de44a3019eb85381a1
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-10-18 00:23:53 +0900

    Update NumberBaseball.jsx

--- lecture/NumberBaseball.jsx
+++ lecture/NumberBaseball.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import Try from './Try';
 
 function getNumbers() {
-    
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), i)[0];
+        array.push(chosen);
+    }
 }
 
 class NumberBaseball extends Component {
@@ -15,6 +20,40 @@ class NumberBaseball extends Component {
 
     onSubmitForm = (e) => {
         e.preventDefault();
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런!',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+
+            })
+        } else {
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if(this.state.tries.length >= 9) { //10번 이상 틀렸을 때
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다!}`,
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value: '',
+                    answer: getNumbers(),
+                    tries: [],
+                });
+            } else {
+                for (let i = 0; i < 4; i += 1) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
+                    value: '',
+                })
+            }
+        }
     };
 
     onChangeInput = (e) => {
