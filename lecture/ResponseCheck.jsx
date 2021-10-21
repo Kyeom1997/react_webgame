280db6de31243a1f90c7e3ffbca9e4d4e944f577
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-10-22 01:15:02 +0900

    Update ResponseCheck.jsx

--- lecture/ResponseCheck.jsx
+++ lecture/ResponseCheck.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 const ResponseCheck = () => {
     const [state, setState] = useState('waiting');
@@ -12,21 +12,21 @@ const ResponseCheck = () => {
         if (state === 'waiting') {
             setState('ready');
             setMessage('초록색이 되면 클릭하세요.');
-            timeout.current = setTimeout(() => {
+            timeOut.current = setTimeout(() => {
                 setState('now');
                 setMessage('지금 클릭하세요!');
-                startTime = new Date();
+                startTime.current = new Date();
             }, Math.floor(Math.random() * 1000) + 2000); //2초~3초 랜덤
         } else if (state === 'ready') { //성급하게 클릭
-            clearTimeout(timeout.current);
+            clearTimeout(timeOut.current);
             setState('waiting');
             setMessage('너무 빨리 누르셨어요! 초록색이 된 후에 클릭하세요.');
         } else if (state === 'now') { //반응속도 체크
-            endTime = new Date();
+            endTime.current = new Date();
             setState('waiting');
             setMessage('클릭해서 시작하세요.');
-            setResult((prevState) => {
-                return [...prevState.result, endTime - startTime]
+            setResult((prevResult) => {
+                return [...prevResult, endTime.current - startTime.current]
             })
         }
     };
