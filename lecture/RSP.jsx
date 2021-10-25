f7edef5cbe3d7ef32044b86b8ce4158cf64d8633
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-10-25 22:19:06 +0900

    Update RSP.jsx

--- lecture/RSP.jsx
+++ lecture/RSP.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// 클래스의 경우 -> constructor -> render -> ref -> componentDidMount 
+// (setState/props 바뀔때 -> shouldComponentUpdate(true) -> render -> componentDidUpdate)
+// 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
 
 class RSP extends Component {
     state = {
@@ -6,6 +9,18 @@ class RSP extends Component {
         imgCoord: 0,
         score: 0,
     };
+
+    componentDidMount() { //컴포넌트가 첫 렌더링된 후
+
+    }
+
+    componentDidUpdate() {//리렌더링 후
+
+    }
+
+    componentWillUnmount() { //컴포넌트가 제거되기 직전
+
+    }
 
     render() {
         const { result, score, imgCoord } = this.state;
