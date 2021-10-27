cc8433c730029cfc943f446019334abedc782e15
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-10-27 22:31:23 +0900

    Update RSP.jsx

--- lecture/RSP.jsx
+++ lecture/RSP.jsx
@@ -18,24 +18,24 @@ const scores = {
 class RSP extends Component {
     state = {
         result: '',
-        imgCoord: 0,
+        imgCoord: '0',
         score: 0,
     };
 
     interval;
 
     componentDidMount() { //컴포넌트가 첫 렌더링된 후
-        const {imgCoord} = this.state;
         this.interval = setInterval(() => {
+            const {imgCoord} = this.state;
             if(imgCoord === rspCoords.바위) {
                 this.setState({
                     imgCoord: rspCoords.가위,
                 });
-            } else if(imgCoord = rspCoords.가위) {
+            } else if(imgCoord === rspCoords.가위) {
                 this.setState({
                     imgCoord: rspCoords.보,
                 });
-            } else if(imgCoord = rspCoords.보) {
+            } else if(imgCoord === rspCoords.보) {
                 this.setState({
                     imgCoord: rspCoords.바위
                 });
@@ -51,15 +51,20 @@ class RSP extends Component {
         clearInterval(this.interval);
     }
 
+    onClickBtn = (choice) => {
+
+    };
+
+
     render() {
         const { result, score, imgCoord } = this.state;
         return (
             <>
             <div id='computer' style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}} />
             <div>
-                <button id="rock" className="btn" onClick={() => onClickBtn('바위')}>바위</button>
-                <button id="scissor" className="btn" onClick={() => onClickBtn('가위')}>가위</button>
-                <button id="paper" className="btn" onClick={() => onClickBtn('보')}>보</button>
+                <button id="rock" className="btn" onClick={() => this.onClickBtn('바위')}>바위</button>
+                <button id="scissor" className="btn" onClick={() => this.onClickBtn('가위')}>가위</button>
+                <button id="paper" className="btn" onClick={() => this.onClickBtn('보')}>보</button>
             </div>
             <div>{result}</div>
             <div>현재 {score}점</div>
