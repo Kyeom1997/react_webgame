955054be4a4281539bc2a43e09d4713e0aeb194d
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-06 03:11:11 +0900

    createContext/Provider

--- lecture/MineSearch.jsx
+++ lecture/MineSearch.jsx
@@ -1,11 +1,19 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, createContext, useMemo } from 'react';
 import Table from './Table';
+import Form from './Form';
+
+export const TableContext = createContext({
+    tableData: [],
+    dispatch: () => {},
+});
 
 const initialState = {
     tableData: [],
     timer: 0,
     result: '',
 };
+
+export const START_GAME = 'START_GAME';
 
 const reducer = (state, action) => {
     switch(action.type) {
@@ -17,13 +25,15 @@ const reducer = (state, action) => {
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const value = useMemo(() => ({tableData: state.tableData, dispatch}), [state.tableData]);
+
     return (
-        <>
+        <TableContext.Provider value={value}>
             <Form />
             <div>{state.timer}</div>
             <Table />
             <div>{state.result}</div>
-        </>
+        </TableContext.Provider>
     );
 };
 
