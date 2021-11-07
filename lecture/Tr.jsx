d51a75e77e347ad74d281018b45855b2efac863d
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-07 21:57:23 +0900

    td tr

--- lecture/Tr.jsx
+++ lecture/Tr.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TableContext } from './MineSearch';
 import Td from './Td';
 
 const Tr = () => {
+    const { tableData } = useContext(TableContext);
+
     return (
         <tr>
-            <Td />
+            {tableData[0] && Array(tableData[0].length).fill().map((td, i) => <Td />)}
         </tr>
     )
 
