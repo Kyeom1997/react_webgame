a6c00d09a899d3a3e38b64350e33dc2ece3af942
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-07 21:29:36 +0900

    Update MineSearch.jsx

--- lecture/MineSearch.jsx
+++ lecture/MineSearch.jsx
@@ -2,8 +2,25 @@ import React, { useReducer, createContext, useMemo } from 'react';
 import Table from './Table';
 import Form from './Form';
 
+export const CODE = {
+    MINE = -7,
+    NORMAL = -1,
+    QUESTION = -2,
+    FLAG: -3,
+    QUESTION_MINE: -4,
+    FLAG_MINE: -5,
+    CLICKED_MINE: -6,
+    OPENED: 0, // 0 이상이면 다 Opened
+};
+
 export const TableContext = createContext({
-    tableData: [],
+    tableData: [
+        [],
+        [],
+        [],
+        [],
+        [],
+    ],
     dispatch: () => {},
 });
 
@@ -13,10 +30,43 @@ const initialState = {
     result: '',
 };
 
+const plantMine = (row, cell, mine) => {
+    console.log(row, cell, mine);
+    const candidate = Array(row * cell).fill().map((arr, i) => {
+        return i;   
+    });
+    const shuffle = [];
+    while (candidate.length > row * cell - mine) {
+        const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+        shuffle.push(chosen);
+    }
+    const data = [];
+    for (let i = 0; i < row; i++) {
+        const rowData = [];
+        data.push(rowData);
+        for (let j = 0; j < cell; j++) {
+            rowData.push(CODE.NORMAL);
+        }
+    }
+
+    for (let k = 0; k < shuffle.length; k++) {
+        const ver = Math.floor(shuffle[k] / cell);
+        const hor = shuffle[k] % cell;
+        data[ver][hor] = CODE.MINE;
+    }
+
+    return data;
+};
+
 export const START_GAME = 'START_GAME';
 
 const reducer = (state, action) => {
     switch(action.type) {
+        case START_GAME:
+            return {
+                ...state,
+                tableData: plantMine(action.row, action.cell, action.mine)
+            };
         default:
             return state;
     }
