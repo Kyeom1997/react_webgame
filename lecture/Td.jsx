c831689e909495696bd378157eccb2735aa7558e
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-11-08 00:15:42 +0900

    Update Td.jsx

--- lecture/Td.jsx
+++ lecture/Td.jsx
@@ -9,9 +9,20 @@ const getTdStyle = (code) => {
             return {
                 background: '#444',
             };
+        case CODE.CLICKED_MINE:
         case CODE.OPENED:
             return {
                 background: 'white',
+            };
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+            return {
+                background: 'yellow',
+            };
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+            return {
+                background: 'red',
             };
         default:
             return {
@@ -26,6 +37,14 @@ const getTdText = (code) => {
             return '';
         case CODE.MINE:
             return 'X';
+        case CODE.CLICKED_MINE:
+            return '펑';
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+            return '!';
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+            return '?';
         default:
             return '';
     }
@@ -35,7 +54,20 @@ const Td = ({ rowIndex, cellIndex }) => {
     const { tableData, dispatch } = useContext(TableContext);
 
     const onClickTd = useCallback(() => {
-        dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.OPENED:
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+            case CODE.QUESTION:
+            case CODE.QUESTION_MINE:
+                return;
+            case CODE.NORMAL: 
+                dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+                return;
+            case CODE.MINE:
+                dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex});
+                return;
+        }
     }, []);
 
     return (
